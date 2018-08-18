e831e70e3de28665195c206c4c57ce11e482b5ff
Hugo Luna <hugo_199@hotmail.com>
2018-08-18 18:20:32 -0500

    Add function comment and likes for section Post

--- home/views.py
+++ home/views.py
@@ -12,7 +12,7 @@ from articulos.models import Articulo
 
 # Forms
 from home.forms import PostForm
-from home.models import Post
+from home.models import Post,ComentarioPost,LikePost
 
 # Models
 
@@ -32,6 +32,95 @@ class HomeView(LoginRequiredMixin, CreateView, ListView):
         context['user'] = self.request.user
         context['profile'] = self.request.user.profile
         return context
+
+
+
+@csrf_exempt
+def createComment(request):
+
+    data = {}
+    if request.is_ajax:
+        comentario = ComentarioPost(user=request.user,profile=request.user.profile,body=request.POST['comment'],post=int(request.POST['post']),comment_child=0)
+        comentario.save()
+        data['message'] = 'success'
+    else:
+        data['message'] = 'error'
+
+
+    return JsonResponse(data)
+
+
+
+"""Likes for post section """
+@csrf_exempt
+def checkLikesPost(request):
+    data = {}
+
+
+    if request.is_ajax():#Comprobar si la petición es via Ajax
+
+        if LikePost.objects.filter(user=request.user,post=int(request.POST['post'])).exists():
+            #Comprobar si existe el objeto de like en la base de datos, si existe se debera eliminar
+
+            LikePost.objects.filter(user=request.user).filter(post=request.POST['post']).delete()
+
+            data['success'] = 'Like eliminado del post %s'%(request.POST['post'])
+            data['status'] = 'delete'
+            data['message'] = 'Hello '
+            countLikes = LikePost.objects.filter(post=request.POST['post']).count()
+
+            data['countLikes'] = countLikes
+
+            return JsonResponse(data)
+
+        else:#En caso contrario que no exista, agregar un nuevo objeto
+            like = LikePost(user=request.user,profile=request.user.profile,post=request.POST['post'])
+            like.save()
+
+            data['success'] = 'Like agregado para el post %s'%(request.POST['post'])
+            data['status'] = 'exist'
+            data['message'] = 'Hello '
+            countLikes = LikePost.objects.filter(post=request.POST['post']).count()
+
+            data['countLikes'] = countLikes
+
+            return JsonResponse(data)
+
+
+    else:#En caso de no serlo, retornar un error en formato json
+        data['error'] = 'Error'
+        data['message'] = 'Hello error'
+
+    # Conteo de likes en la publicacion
+
+
+    return JsonResponse(data)
+
+
+
+@csrf_exempt
+def checkLikesCountPost(request):
+    data = {}
+    if request.is_ajax():  # Comprobar si la petición es via Ajax
+
+        if LikePost.objects.filter(user=request.user, post=int(request.POST['post'])).exists():
+            data['status'] = 'exist'
+
+        else:
+            data['status'] = 'no'
+
+    countLikes = LikePost.objects.filter(post=request.POST['post']).count()
+
+    data['countLikes'] = countLikes
+
+    return JsonResponse(data)
+
+
+
+
+
+
+
 
 
 
